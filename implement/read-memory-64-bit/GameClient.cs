b59f6ca45f5dee0bc38d1ca90dad0723aa49e10a
Lak Moore <LakMoore@after-ice.com>
2025-07-01 17:05:16 +0100

    Add a tidy system for cacheing uiTree addresses once found

---
+++ implement/read-memory-64-bit/GameClient.cs
@@ -0,0 +1,10 @@
+﻿namespace read_memory_64_bit
+{
+    public record GameClient
+    {
+        public string? mainWindowTitle;
+        public required int processId;
+        public required long mainWindowId;
+        public ulong uiRootAddress;
+    }
+}
